45725dad10df071fe7cc96111108ce6d6a37cb3b
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2025-06-15 18:29:53 +0100

    removed tailwindcss from project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export default function App() {
   return (
-    <div className='min-h-screen flex items-center justify-center bg-gray-100'>
-      <h1 className='text-4xl font-bold text-red-600'>Tailwind is working!</h1>
+    <div>
+      <h1>FitFuel</h1>
     </div>
   );
 }
